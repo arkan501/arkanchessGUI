350916e33206f83aa9786d33926ef5a47ce2ee26
arkan <arkan501@proton.me>
2024-11-27 07:15:48 -0700

    fix: board and pieces scale correctly
    After a lot of fiddling, and a lot of help from Codeium, I was able to
    build the layout for the piece cells correctly. The board background can
    be used properly, and the pieces are placed on them properly. Also, the
    pieces scale correctly dependent on the window aspect ratio. This will
    most likely need to be revisited in the future, when more elements are
    added to the App. For now, it works well enough that I can move on to
    implementing the functions to get the game to now actually work.

--- layout.go
+++ layout.go
@@ -2,25 +2,29 @@ package main
 
 import (
 	"fyne.io/fyne/v2"
-    // "fyne.io/fyne/v2/canvas"
+    "fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/theme"
 )
 
 type chessLayout struct {
+    boardImage *canvas.Image
 }
 
-// TODO: figure out what this is doing exactly
 func (c *chessLayout) Layout(cells []fyne.CanvasObject, size fyne.Size) {
-	squareSize := size.Width / 8
+    squareSize := fyne.Min(size.Width, size.Height) / 8
+    totalSize := squareSize * 8
+    offsetx := (size.Width - totalSize) / 2
+    offsety := (size.Height - totalSize) / 2
 
 	for ix, piece := range cells {
-		x := float32(ix%8) * squareSize
-		y := float32(ix/8) * squareSize
+		x := float32(ix%8) * squareSize + offsetx
+		y := float32(ix/8) * squareSize + offsety
 		piece.Resize(fyne.NewSize(squareSize, squareSize))
 		piece.Move(fyne.NewPos(x, y))
 	}
 }
 
+// Should I even have these?
 func (b *chessLayout) MinSize([]fyne.CanvasObject) fyne.Size {
 	edge := theme.IconInlineSize() * 8
 	return fyne.NewSize(edge, edge)
